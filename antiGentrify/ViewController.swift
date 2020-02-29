09cf695dd64ecd78aa908445d49b3aeb8714a89d
George Martin <georgeg10live@gmail.com>
2020-02-29 04:39:00 -0500

    initialize xcode

---
+++ antiGentrify/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  antiGentrify
+//
+//  Created by George Martin on 2/29/20.
+//  Copyright © 2020 Developer Student Club Morgan State. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
